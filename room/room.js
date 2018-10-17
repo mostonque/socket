1f444ba21f4e19f384e2530e11f074c9a776c8cd
SERHAT PEKEDİS <serhatpekedis@gmail.com>
2018-10-17 17:36:54 +0300

    Socket io Anlatımı bitti

--- room/room.js
+++ room/room.js
@@ -11,9 +11,27 @@ io.on('connection',(socket)=>{
 
     socket.on('joinRoom',(data)=>{
       socket.join(data.name,()=>{
-          let count=io.sockets.adapter.rooms[data.name].length;
-          io.to(data.name).emit('new join',{count});
+          io.to(data.name).emit('new join',{count:getOnlineCount(io,data)});
           socket.emit('me',{name:data.name});
       });
     });
+
+    socket.on('leaveRoom',(data)=>{
+        socket.leave(data.name,()=>{
+        io.to(data.name).emit('leavedRoom',{count:getOnlineCount(io,data)});
+        socket.emit('leaveMsg',{name:data.name});
+        })
+    });
 });
+
+const getOnlineCount=(io,data)=> {
+    const room=io.sockets.adapter.rooms[data.name];
+    //daha kısa kod hali şu
+    // return room ? room.lenght :0
+    //yani room varsa room varsa room.lenght i bana ver yoksa 0 döndür
+    if (room){
+        return room.length;
+    } else{
+        return 0;
+    }
+};
